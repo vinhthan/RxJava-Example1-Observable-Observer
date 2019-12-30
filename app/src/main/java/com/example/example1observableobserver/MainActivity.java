a004260f9e92655c580110013ef1a29110a6da5c
thanmanhvinh <thanmanhvinh@gmail.com>
2019-12-30 16:30:41 +0700

    commit

---
+++ app/src/main/java/com/example/example1observableobserver/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.example1observableobserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //observable
+        Observable<String> footballPlayesObservable = getFootballPlayesObservable();
+
+        //observer
+        Observer<String> footbalPlayesObserver = getFootballPlayesObserver();
+
+        //observer subcribing to observable
+        footballPlayesObservable.observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(footbalPlayesObserver);
+    }
+
+    private Observer<String> getFootballPlayesObserver() {
+        return  new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: "+s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: "+e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+    }
+
+    //tao Observable de phat dlieu
+    private Observable<String> getFootballPlayesObservable(){
+        return Observable.just("Ronaldo, Messi, Quang Hai, Tuan Anh, Cong Phuong, Tien Linh, Minh Vuong, Tien Dung, Van Lam, Van Quyet, Trong Hoang");
+    }
+
+
+}
